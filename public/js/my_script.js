c77bb41c0fd99170db1e7df3333240dec02e0b86
worrawut <pworrawut@hotmail.com>
2015-03-12 17:16:00 +0700

    modal for images

--- public/js/my_script.js
+++ public/js/my_script.js
@@ -17,4 +17,17 @@ $(function(){
 	// tooltip for artists thumbnail
 	$('[data-toggle="tooltip"]').tooltip({ animation: true });
 
+	// Show modals
+	$('.modalphotos img').on('click', function(){
+		$('#modal').modal({
+			show: true
+		})
+
+		var mysrc = this.src.substr(0, this.src.length-7) + '.jpg';
+		$('#modalimage').attr('src', mysrc);
+		$('#modalimage').on('click', function(){
+			$('#modal').modal('hide');
+		});
+	});
+
 }); // jQuery is loaded
