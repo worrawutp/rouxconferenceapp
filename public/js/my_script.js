616da5d53499b7d0fdfa8f9605695ec6787b482a
worrawut <pworrawut@hotmail.com>
2015-04-01 22:11:02 +0700

    finalize look

--- public/js/my_script.js
+++ public/js/my_script.js
@@ -10,6 +10,20 @@ $(function(){
 	$("#artists a:contains('Artists')").parent().addClass('active');
 	$("#register a:contains('Register')").parent().addClass('active');
 
+	// controlling opening of accordions
+	$('#monday.collapse').addClass('in');
+
+	$('#artists #tuesday.collapse').addClass('in');
+	$('#artists #wednesday.collapse').addClass('in');
+	$('#artists #friday.collapse').addClass('in');
+
+	$('#register #monday.collapse').removeClass('in');
+
+	// add bootstrap classes to photos
+	$('.abouttheartists img').addClass('img-circle');
+	$('aside.photosformlastyear img').addClass('img-thumbnail');
+	$('.artistinfo .photogrid img').addClass('img-circle');
+
 	// Make menu drop automatically
 	$('ul.nav li.dropdown').hover(function(){
 		  $('.dropdown-menu', this).fadeIn();
